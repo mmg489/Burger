d5ef5bcd4ae0219ed97879459d97de8dae7ecdee
Margarita <m_mg489@outlook.com>
2019-05-31 19:03:11 -0400

    updated seeds.sql with test burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", TRUE);
+INSERT INTO burgers (burger)name, devoured) VALUES ("Big Mac", FALSE);
