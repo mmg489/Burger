cfb265de15be3ded5f63bbd35f7c1ef9357f52f3
Margarita <m_mg489@outlook.com>
2019-05-31 19:43:32 -0400

    changed BOOLEAN value in line 2 seeds sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", FALSE);
