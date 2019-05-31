4ecd11ddb30d9251f8a9ee41e7c64beb54ba2f60
Margarita <m_mg489@outlook.com>
2019-05-31 19:20:59 -0400

    corrected syntax error in line 3 of seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey", FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", TRUE);
-INSERT INTO burgers (burger)name, devoured) VALUES ("Big Mac", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", FALSE);
