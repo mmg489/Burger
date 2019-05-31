c24bfaae40aa03540605b7f1bb25aba6f442b5dc
Margarita <m_mg489@outlook.com>
2019-05-31 19:14:26 -0400

    notes for setting up PORT

--- server.js
+++ server.js
@@ -3,3 +3,19 @@ var express = require('express');
 var PORT = process.env.PORT || 8080;
 
 var app = express ();
+
+// Serves static content for the app from the "public" directory in the application directory
+
+
+//Parse application body as JSON
+
+
+// Set Handlebars 
+
+
+// Import routes and give the server access to them.
+
+//Start server so that it can begin listening to client requests.
+
+    //Log (server-side) when server has started
+    console.log("Server listening on: http://localhost:" + PORT);
