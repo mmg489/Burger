d434f1da9ebb2ff7b21c5cc40c482e952e39e45a
Margarita <m_mg489@outlook.com>
2019-06-03 00:22:34 -0400

    correction of syntax errors

--- config/orm.js
+++ config/orm.js
@@ -7,8 +7,8 @@ var orm = {
     all: function (tableInput, cb) {
         connection.query("SELECT * FROM " + tableInput + ";", function (err,result) {
             if (err) throw err;
-            cb(result)
-        })
+            cb(result);
+        });
     },
     //UPDATE METHOD
     update: function (tableInput, condition, cb) {
