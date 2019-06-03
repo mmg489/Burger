bcdd06b64086aa016031c51bfdbf42d6079b535b
Margarita <m_mg489@outlook.com>
2019-06-03 01:10:37 -0400

    added burger options to seeds. Corrected syntax issue in schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Turkey", FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Soy Bean Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Bean Burger", FALSE);
