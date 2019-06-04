d86b460c81b6f627ea13c0bfb9639c69a3f91ff2
Margarita <m_mg489@outlook.com>
2019-06-03 20:30:16 -0400

    updated connection var to attempt to correct connection issues

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,10 @@ var mysql = require("mysql");
 //to make mySQL connection
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
 });
 
 //to establish connection
